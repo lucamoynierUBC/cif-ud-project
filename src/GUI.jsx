8bac3026a10e6cd35ff124d2fa77554b5a0da31a
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-06-29 14:28:37 -0400

    added button -->color changer

--- src/GUI.jsx
+++ src/GUI.jsx
@@ -1,16 +1,23 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
+import useApp from "./stores/useApp"
 
 export default function GUI() {
 
-    
+    // state contains our store information
+    const changeColor = useApp((state) => state.changeColor)
+    console.log(changeColor)
 
+    const buttonClick = () => {
+        console.log("button clicked")
+        changeColor()
+    }
 
 
 
     return <div className="gui">
         {/* testButton */}
         <div>
-            <button className="testButton">Click Me</button>
+            <button className="testButton" onClick={buttonClick}>Click Me</button>
         </div>
 
     </div>
--- src/stores/useApp.jsx
+++ src/stores/useApp.jsx
@@ -3,8 +3,25 @@
 import { create } from "zustand"
 
 // Im thinking this is how I will be able to share information from the UI and the 3D scene
-export default create(() => {
+export default create((set) => {
     return {
-        color: 'teal'
+        color: "purple",
+        changeColor: () => {
+            const randomColor = getRandomColor()
+            set(() => {
+                return {color: randomColor}
+
+            })
+        }
     }
 })
+
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}

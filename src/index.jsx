3e1a8a89e9a594c08d78dcbe44a537d8c550c936
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-07-14 09:25:37 -0400

    Basic loading screen added

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import GUI from './GUI'
+import { Loader } from '@react-three/drei'
+import { Suspense } from 'react'
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
     {/*//everything related to R3F goes into canvas, GUI goes outside  */}
         <Canvas>
-            <Experience />
+            <Suspense fallback={null}>
+                <Experience />
+            </Suspense>
         </Canvas>
         <GUI></GUI>
+        <Loader></Loader>
 
         
     </>

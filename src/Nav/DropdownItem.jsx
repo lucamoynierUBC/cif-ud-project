78acbb65ea67b64e176895d76c5f55ee19ea1561
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-07-31 15:38:07 -0400

    added caret icon

--- src/Nav/DropdownItem.jsx
+++ src/Nav/DropdownItem.jsx
@@ -1,9 +1,9 @@
 export default function DropdownItem(props) {
     return(
         <a href="#" className="menu-item">
-            <span className="icon-button">{props.leftIcon}</span>
+            {/* <span className="icon-button">{props.leftIcon}</span> */}
             {props.children}
-            <span className="icon-right">{props.rightIcon}</span>
+            {/* <span className="icon-right">{props.rightIcon}</span> */}
         </a>
 
     )
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,14 +1,18 @@
+import React from "react";
 import DropdownMenu from "./DropdownMenu";
 import NavBar from "./NavBar";
 import NavItem from "./NavItem";
 import NavText from "./NavText";
+
 
 export default function Nav() {
     return(
         <NavBar>
             <NavText text="City of Yes"/>
             <NavText text="Urban Design"/>
-            <NavItem icon=">">
+            <NavItem icon={<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 16 10">
+    <path d="M15.434 1.235A2 2 0 0 0 13.586 0H2.414A2 2 0 0 0 1 3.414L6.586 9a2 2 0 0 0 2.828 0L15 3.414a2 2 0 0 0 .434-2.179Z"/>
+  </svg>}>
                 <DropdownMenu />
             
             </NavItem>

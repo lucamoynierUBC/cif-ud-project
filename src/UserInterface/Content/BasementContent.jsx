f05dc170dfcb1cd7f1691ae3a2b644f35e92ea42
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-08-01 16:56:45 -0400

    Interaction observer dependencies added + test

--- src/UserInterface/Content/BasementContent.jsx
+++ src/UserInterface/Content/BasementContent.jsx
@@ -1,7 +1,24 @@
+import { useRef, useEffect, useState } from "react"
+import { useInView } from "react-intersection-observer"
+
 export default function BasementContent() {
+    const { ref: myRef, inView: visible} = useInView()
+    
+    // const myRef = useRef()
+    // const [visible, setVisible] = useState()
+
+    console.log('Visible? ', visible)
+    // useEffect(() => {
+    //     const observer = new IntersectionObserver ((entries) => {
+    //         const entry = entries[0]
+    //         setVisible(entry.isIntersecting)
+    //     })
+    //     observer.observe(myRef.current)
+    // }, [])
+
     return(
         <div>
-            <h2>Basement Adu</h2>
+            <h2 ref={myRef}>Basement Adu</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc id cursus metus.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sociis natoque penatibus et magnis dis parturient montes.</p>
         </div>

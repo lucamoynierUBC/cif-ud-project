a721f98af8f2364d1f98550a7df9a8543015eb5f
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-08-04 12:31:54 -0400

    Added spawner

--- src/Spawner.jsx
+++ src/Spawner.jsx
@@ -2,10 +2,25 @@ import { PointMaterialImpl } from "@react-three/drei";
 import Birds from "./Birds";
 import Person from "./Person";
 import Bus from "./Bus";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useGUI from "./stores/useGUI";
 
 export default function Spawner(){
-    const [spawn, setSpawn] = useState(true)
+    const [spawn, setSpawn] = useState(false)
+
+    useEffect(() => {
+        const unsubscribeSpawn = useGUI.subscribe(
+            (state) => state.guiIntroPhase,
+              (guiIntroPhase) => {
+                if (guiIntroPhase === 'off'){
+                  setSpawn(true)
+                }
+              }
+        )
+        return () => {
+            unsubscribeSpawn()
+        }
+    },[])
     return(
         spawn && <group>
             <Birds></Birds>

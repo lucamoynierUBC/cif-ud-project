ed860bb4689dad0a9c0b84455a795fad4bff3071
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-11-30 10:45:36 -0800

    adjusted loading screen

--- src/UserInterface/Components/LoadingScreen.jsx
+++ src/UserInterface/Components/LoadingScreen.jsx
@@ -1,10 +1,14 @@
 import { useProgress } from "@react-three/drei";
-import { Progress } from "antd";
+import { Flex, Progress, Spin, Statistic } from "antd";
+import { useMemo } from "react";
+import md5 from "md5";
 
 export default function LoadingScreen() {
-    const { progress, item } = useProgress();
+    const { progress, item, loaded } = useProgress();
 
     const twoColors = { '0%': '#ffeec1', '100%': '#d96b27' };
+
+    const roundedProgress = parseFloat(progress.toFixed(2));
 
     const containerStyle = {
         position: 'absolute',
@@ -16,12 +20,17 @@ export default function LoadingScreen() {
         minWidth: '300px', // Adjust for minimum width
     };
 
+    console.log(loaded)
+    const itemHash = useMemo(() => item ? md5(item).substring(0, 8) : '', [item]);
+
     return (
         <div style={containerStyle}>
-            <Progress percent={progress}  strokeColor={twoColors}/>
-            <div style={{ marginTop: '10px' }}>
-                {item ? `Loading: ${item}` : 'Starting up...'}
-            </div>
+            <Progress percent={Math.round(roundedProgress)}  strokeColor={twoColors}/>
+            <Flex justify="center" >
+                <div  style={{ marginTop: '10px' }}>
+                    <Statistic title="Importing assets:" value={loaded} suffix="/ 20"/>
+                </div>
+            </Flex>
 
         </div>
     );
